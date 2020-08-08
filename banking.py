f260ddaf73fc243f6f3017812a160dd7904aab0e
Shivang Medheakar <69140290+shivangmedhekar@users.noreply.github.com>
2020-08-08 15:27:38 +0530

    Add files via upload

---
+++ banking.py
@@ -0,0 +1,138 @@
+from abc import ABCMeta, abstractmethod
+from random import randint
+
+#Abstract Class
+class Account(metaclass = ABCMeta):
+
+    @abstractmethod
+    def createAccount():
+        return 0
+
+    @abstractmethod
+    def authenticate():
+        return 0
+
+    @abstractmethod
+    def withdraw():
+        return 0
+
+    @abstractmethod
+    def deposit():
+        return 0
+
+    @abstractmethod
+    def displayBalance():
+        return 0
+
+
+class SavingsAccount(Account):
+    def __init__(self):
+
+        # [key][0] => name ; [key][1] => balance
+        self.savingsAccounts = {}
+
+    def createAccount(self, name, initialDeposit):
+        print()
+        self.accountNumber = randint(10000, 99999)
+        self.savingsAccounts[self.accountNumber] = [name, initialDeposit]
+        print("Account creation has been successful. Your account number is ", self.accountNumber)
+        print()
+
+    def authenticate(self, name, accountNumber):
+        print()
+        if accountNumber in self.savingsAccounts.keys():
+            if self.savingsAccounts[accountNumber][0] == name:
+                print("Authentication Successful")
+                self.accountNumber = accountNumber
+                print()
+                return True
+            else:
+                print("Authentication Failed")
+                print()
+                return False
+        else:
+            print("Authentication Failed")
+            print()
+            return False
+
+    def withdraw(self, withdrawalAmount):
+        print()
+        if withdrawalAmount > self.savingsAccounts[self.accountNumber][1]:
+            print("Insufficient balance")
+        else:
+            self.savingsAccounts[self.accountNumber][1] -= withdrawalAmount
+            print("Withdrawal was successful.")
+            self.displayBalance()
+        print()
+
+    def deposit(self, depositAmount):
+        print()
+        self.savingsAccounts[self.accountNumber][1] += depositAmount
+        print("Deposit was successful.")
+        self.displayBalance()
+        print()
+
+    def displayBalance(self):
+        print("Avaialble balance: ",self.savingsAccounts[self.accountNumber][1])
+
+savingsAccount = SavingsAccount()
+while True:
+
+    print()
+    print("Enter 1 to create a new account")
+    print("Enter 2 to access an existing account")
+    print("Enter 3 to exit")
+
+    userChoice = int(input())
+    print()
+
+    if userChoice is 1:
+
+        print()
+        name = input("Enter your name: ")
+        deposit = int(input("Enter the initial deposit: "))
+        savingsAccount.createAccount(name, deposit)
+    
+        print()
+
+    elif userChoice is 2:
+        print()
+
+        name = input("Enter your name: ")
+        accountNumber = int(input("Enter your account number: "))
+        authenticationStatus = savingsAccount.authenticate(name, accountNumber)
+        print()
+
+        if authenticationStatus is True:
+            while True:
+                print()
+                print("Enter 1 to withdraw")
+                print("Enter 2 to deposit")
+                print("Enter 3 to display avialable balance")
+                print("Enter 4 to go back to the previous menu")
+                userChoice = int(input())
+
+                print()
+                if userChoice is 1:
+                    print()
+
+                    withdrawalAmount = int(input("Enter a withdrawal amount"))
+                    savingsAccount.withdraw(withdrawalAmount)
+                    print()
+
+                elif userChoice is 2:
+                    print()
+
+                    depositAmount = int(input("Enter an amount to be deposited"))
+                    savingsAccount.deposit(depositAmount)
+                    print()
+
+                elif userChoice is 3:
+                    print()
+                    savingsAccount.displayBalance()
+                    print()
+                    
+                elif userChoice is 4:
+                    break
+    elif userChoice is 3:
+        quit()
